7714cd42421458ce03d4ef31a3ac35a4569dfcb0
Artur Avtukhov <archy6754267@gmail.com>
2022-08-08 21:33:44 +0200

    Final

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,26 @@
-
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
+const saveButton = popup.querySelector('.popup__save-info');
 const closeEditButton = popup.querySelector('.popup__close');
 const profileInfoName = document.querySelector('.profile__info_name');
 const profileInfoActivity = document.querySelector('.profile__info_activity');
-
 const popupEditName = popup.querySelector('.popup__edit-name');
 const popupEditActivity = popup.querySelector('.popup__edit-activity');
 
 
-console.log(profileInfoName.textContent);
-console.log(popupEditName.textContent);
-
-
 const togglePopup = function () {
   popup.classList.toggle('popup_opened');
-
   popupEditName.value = profileInfoName.textContent;
   popupEditActivity.value = profileInfoActivity.textContent;
-
 }
 
-editButton.addEventListener('click', togglePopup)
-closeEditButton.addEventListener('click', togglePopup)
+editButton.addEventListener('click', togglePopup);
+closeEditButton.addEventListener('click', togglePopup);
 
+const savePopup = function () {
+  popup.classList.toggle('popup_opened');
+  profileInfoName.textContent = popupEditName.value;
+  profileInfoActivity.textContent = popupEditActivity.value;
+}
 
-// profileInfoName.innerHTML = `
-// <div class="profile__info_group">
-//   <p class="profile__info_name">${profileInfoName.textContent}</p>
-// </div>
-// `;
+saveButton.addEventListener('click', savePopup);
