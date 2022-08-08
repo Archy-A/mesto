43969bb9caad1eb8274252e60bacb771787bf41f
Artur Avtukhov <archy6754267@gmail.com>
2022-08-08 21:55:56 +0200

    Website_tests02

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const saveButton = popup.querySelector('.popup__save-info');
 const closeEditButton = popup.querySelector('.popup__close');
-const profileInfoName = document.querySelector('.profile__info_name');
-const profileInfoActivity = document.querySelector('.profile__info_activity');
+const profileInfoName = document.querySelector('.profile__name');
+const profileInfoActivity = document.querySelector('.profile__activity');
 const popupEditName = popup.querySelector('.popup__edit-name');
 const popupEditActivity = popup.querySelector('.popup__edit-activity');
 
