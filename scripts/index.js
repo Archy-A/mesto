60940fb2ede4983083af137d41f2736005af7a2d
Artur Avtukhov <archy6754267@gmail.com>
2022-08-10 16:43:07 +0200

    review02_v001

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
-const saveButton = popup.querySelector('.popup__save-info');
+const formSubmit = popup.querySelector('.popup__content');
 const closeEditButton = popup.querySelector('.popup__close');
 const profileInfoName = document.querySelector('.profile__name');
 const profileInfoActivity = document.querySelector('.profile__activity');
@@ -9,19 +9,24 @@ const popupEditActivity = popup.querySelector('.popup__edit-activity');
 
 
 const togglePopup = function () {
-  popup.classList.toggle('popup_opened');
-  popupEditName.value = profileInfoName.textContent;
-  popupEditActivity.value = profileInfoActivity.textContent;
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.toggle('popup_opened');
+  }
+  else {
+    popup.classList.toggle('popup_opened');
+    popupEditName.value = profileInfoName.textContent;
+    popupEditActivity.value = profileInfoActivity.textContent;
+  }
 }
 
 editButton.addEventListener('click', togglePopup);
 closeEditButton.addEventListener('click', togglePopup);
 
-const savePopup = function (evt) {
+const saveProfile = function (evt) {
   evt.preventDefault();
-  popup.classList.toggle('popup_opened');
   profileInfoName.textContent = popupEditName.value;
   profileInfoActivity.textContent = popupEditActivity.value;
+  togglePopup();
 }
 
-saveButton.addEventListener('click', savePopup);
+formSubmit.addEventListener('submit', saveProfile);
