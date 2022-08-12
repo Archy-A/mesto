1bc3d5f024e3b2aab05f63c293a36eed1cc33c88
Artur Avtukhov <archy6754267@gmail.com>
2022-08-12 09:13:37 +0200

    review03_v01

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,16 @@ const formSubmit = popup.querySelector('.popup__content');
 const closeEditButton = popup.querySelector('.popup__close');
 const profileInfoName = document.querySelector('.profile__name');
 const profileInfoActivity = document.querySelector('.profile__activity');
-const popupEditName = popup.querySelector('.popup__edit-name');
+const popupEditName = popup.querySelector('.popup__edit_name_copy');
 const popupEditActivity = popup.querySelector('.popup__edit-activity');
 
 
 const togglePopup = function () {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.toggle('popup_opened');
-  }
-  else {
-    popup.classList.toggle('popup_opened');
+  if (!popup.classList.contains('popup_opened')) {
     popupEditName.value = profileInfoName.textContent;
     popupEditActivity.value = profileInfoActivity.textContent;
   }
+  popup.classList.toggle('popup_opened');
 }
 
 editButton.addEventListener('click', togglePopup);
